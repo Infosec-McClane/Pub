95c450b901f5c3c8c8158c8048d0610fe87c8cef
InfoSec_McClane <kurt.muhl@gmail.com>
2020-08-07 11:25:06 -0500

    Delete iframe.js

--- iframe.js
+++
@@ -1,4 +0,0 @@
-var iframe = document.createElement('iframe');
-iframe.style.display = "none";
-iframe.src = 'https://qsdev.contractexpress.com/_login/extranetlogin.aspx';
-document.body.appendChild(iframe);
